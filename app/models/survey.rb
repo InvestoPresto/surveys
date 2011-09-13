5601b3160c33adb61bb06c71655f101388db4dab
Subhash Chandra <tmayad@loonyb.in>
2011-09-13 14:56:03 +0530

    cosmetic fixes

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,4 +1,4 @@
 class Survey < ActiveRecord::Base
-  has_many :questions, :dependent => :destroy
+  has_many :questions, :dependent => :destroy, :order => :id
   accepts_nested_attributes_for :questions, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
