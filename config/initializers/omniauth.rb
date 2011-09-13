ee2097967b39a678ea309723519301e832402c7d
Subhash Chandra <tmayad@loonyb.in>
2011-09-13 16:18:56 +0530

    added facebook authentication and related buttons

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,6 @@
+require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :facebook, ENV['facebook_key'], ENV['facebook_secret'] if ENV['facebook_key'] && ENV['facebook_secret']
   provider :twitter, ENV['twitter_key'], ENV['twitter_secret'] if ENV['twitter_key'] && ENV['twitter_secret']
+  provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
