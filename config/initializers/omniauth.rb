0bc7b05d26570b094dd62a9f3c253ccbd800e6de
Subhash Chandra <tmayad@loonyb.in>
2011-09-15 14:10:50 +0530

    reinstated yahoo login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['facebook_key'], ENV['facebook_secret'] if ENV['facebook_key'] && ENV['facebook_secret']
   provider :twitter, ENV['twitter_key'], ENV['twitter_secret'] if ENV['twitter_key'] && ENV['twitter_secret']
   provider :google_apps, OpenID::Store::Filesystem.new('/tmp'), :domain => 'gmail.com'
+  provider :open_id, OpenID::Store::Filesystem.new('/tmp')
 end
